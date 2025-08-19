f2a3a67685fc72e28d8442ffb1955539f415ab20
sohampirale <sohampirale20504@gmail.com>
2025-08-19 17:57:11 +0530

    created dockerfile for BE

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 
 const app = express();
+const { PrismaClient } =require('@prisma/client')
 
-app.get('/',(req,res)=>{
-	res.send("Hello World");
+const prisma = new PrismaClient()
+
+app.get('/',async(req,res)=>{
+	const users = await prisma.user.findMany();
+	res.status(200).json({
+		users
+	})
 })
 
+app.post("/",async(req,res)=>{
+	await prisma.user.create({
+		data:{
+			username:Math.random().toString(),
+			Password:Math.random().toString()
+		}
+	})
+
+	res.send('user created')
+})
 
 app.listen(3000,()=>{
 	console.log("server listening on port 3000");
